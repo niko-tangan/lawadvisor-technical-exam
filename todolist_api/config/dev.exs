80e693f400584162b6976be2a37dd291098ad124
Niko Tangan <nikotangan@gmail.com>
2024-09-10 13:47:33 +0800

    feat: add sqlite to be used if in dev (postgres if not)

--- todolist_api/config/dev.exs
+++ todolist_api/config/dev.exs
@@ -5,7 +5,7 @@ config :todolist_api, TodolistApi.Repo,
   username: "postgres",
   password: "postgres",
   hostname: "localhost",
-  database: "todolist_api_dev",
+  database: "todolist_api_dev.sqlite",
   stacktrace: true,
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
--- todolist_api/lib/todolist_api/repo.ex
+++ todolist_api/lib/todolist_api/repo.ex
@@ -1,5 +1,9 @@
 defmodule TodolistApi.Repo do
   use Ecto.Repo,
     otp_app: :todolist_api,
-    adapter: Ecto.Adapters.Postgres
+    adapter:
+      if(Mix.env() in [:dev, :test],
+        do: Ecto.Adapters.SQLite3,
+        else: Ecto.Adapters.Postgres
+      )
 end
