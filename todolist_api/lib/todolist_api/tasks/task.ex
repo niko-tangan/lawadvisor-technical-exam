2670b9925317bf5b9ffb7d43e8f95c49e666b90d
Niko Tangan <nikotangan@gmail.com>
2024-09-11 18:05:46 +0800

    feat: remove required for custom_order, default it to current id

--- todolist_api/lib/todolist_api/tasks/task.ex
+++ todolist_api/lib/todolist_api/tasks/task.ex
@@ -1,6 +1,7 @@
 defmodule TodolistApi.Tasks.Task do
   use Ecto.Schema
   import Ecto.Changeset
+  import Ecto.Query
 
   schema "tasks" do
     field :description, :string
@@ -12,9 +13,19 @@ defmodule TodolistApi.Tasks.Task do
   end
 
   @doc false
+  defp set_custom_order_if_missing(%Ecto.Changeset{data: %TodolistApi.Tasks.Task{custom_order: nil}} = changeset) do
+    query = from t in TodolistApi.Tasks.Task, select: t.id, order_by: [{:desc, :inserted_at}], limit: 1
+    most_recent_id = TodolistApi.Repo.one(query) + 1
+
+    changeset
+    |> put_change(:custom_order, most_recent_id)
+  end
+
+  @doc false
   def changeset(task, attrs) do
     task
     |> cast(attrs, [:description, :is_completed, :deleted_at, :custom_order])
-    |> validate_required([:description, :is_completed, :custom_order])
+    |> set_custom_order_if_missing()
+    |> validate_required([:description, :is_completed])
   end
 end
